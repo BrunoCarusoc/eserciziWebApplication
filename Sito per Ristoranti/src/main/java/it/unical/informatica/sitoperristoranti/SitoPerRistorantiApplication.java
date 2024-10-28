4c7fcd9b04be6c5ebccc42188fc20e427d3455e1
BrunoCarusoc <brunocarusoc@gmail.com>
2024-10-28 18:12:02 +0100

    Creazione Progetto

---
+++ Sito per Ristoranti/src/main/java/it/unical/informatica/sitoperristoranti/SitoPerRistorantiApplication.java
@@ -0,0 +1,13 @@
+package it.unical.informatica.sitoperristoranti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoPerRistorantiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitoPerRistorantiApplication.class, args);
+	}
+
+}
---
+++ Sito per Ristoranti/src/test/java/it/unical/informatica/sitoperristoranti/SitoPerRistorantiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.informatica.sitoperristoranti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitoPerRistorantiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
